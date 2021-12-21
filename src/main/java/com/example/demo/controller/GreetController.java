daea5cb125f10078099f64a92a92a11f96139536
KunigiMohan <kmohanr5@gmail.com>
2021-12-21 12:13:14 +0530

    Added : API added to message by Id value.

--- src/main/java/com/example/demo/controller/GreetController.java
+++ src/main/java/com/example/demo/controller/GreetController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
@@ -18,17 +21,23 @@ import com.example.demo.service.IGreetingService;
  * @RequestMethod : to define method.
  * @RequestParam : Requesting Parameter in URL.
  * @PathVariable : to get value from URL of a Variable.
+ * @GetMapping : to map URL to Particular API
  *
  */
 @RestController
+@RequestMapping("/greeting")
 public class GreetController {
 	public static final String template = "Hello, %s";
 	
 	@Autowired
 	private IGreetingService greetingService;
 	
-	@PostMapping("/greeting/post")
+	@PostMapping("/post")
 	public Greeting greeting(@RequestBody User user) {
 		return greetingService.addGreeting(user);
+	}
+	@GetMapping("/get/Id/{id}")
+	public Greeting gretting2(@PathVariable Long id){
+		return greetingService.getGreetingById(id);
 	}
 }
