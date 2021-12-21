6347d475e2d17330c93e23f1bdb3f7aa9000552e
KunigiMohan <kmohanr5@gmail.com>
2021-12-21 11:18:43 +0530

    Added : Main class of Application.

--- src/main/java/com/example/demo/GreetingControllerApplication.java
+++ src/main/java/com/example/demo/GreetingControllerApplication.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
- * 
- * @SpringBootApplication : Main Class of Spring Boot Application
- *
+ * @SpringBootApplication : main class of application
  */
 @SpringBootApplication
 public class GreetingControllerApplication {
