8809c46be97b3e375c8216ca651a2171e6084069
KunigiMohan <kmohanr5@gmail.com>
2021-12-21 11:14:28 +0530

    Added : Implementing GreetingRepository and defining methods.

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -2,23 +2,31 @@ package com.example.demo.service;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
+import com.example.demo.Repository.GreatingRepository;
 /**
  * 
- * @Service : to Create GreetingService Class 
+ * @Service : to Create GreetingService Class
+ * @Autowired : to crete new object of class 
+ * @Override : overriding method
  *
  */
 @Service
 public class GreetingService implements IGreetingService {
 	public static final String template = "Hello  %s";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreatingRepository greetingRepository;
+	
 	@Override
 	public Greeting addGreeting(User user) {
 		String message = String.format(template, (user.toString().isEmpty()) ? " World " : user.toString());
-		return (new Greeting(counter.incrementAndGet(),message));
+		return greetingRepository.save(new Greeting(counter.incrementAndGet(),message));
 	}
 		
 }
