dc6109c2a7fa4063da164bd4e937507190ce8969
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-12-23 22:44:44 +0530

    Added : Created API to Delete Greeting message by ID.

--- src/main/java/com/example/demo/controller/GreetController.java
+++ src/main/java/com/example/demo/controller/GreetController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
-import com.example.demo.Repository.GreatingRepository;
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
 import com.example.demo.service.IGreetingService;
@@ -31,37 +29,36 @@ import com.example.demo.service.IGreetingService;
  *
  */
 @RestController
-@RequestMapping("/greeting")
+@RequestMapping("/greeting/")
 public class GreetController {
 	public static final String template = "Hello, %s";
 	
 	@Autowired
 	private IGreetingService greetingService;
-
-	@Autowired
-	private GreatingRepository greetingRepository;
-
+		
 	/**
 	 * API for Adding message to Repository
 	 * @param user
-	 * @return
+	 * @return creted user Object
 	 */
 	@PostMapping("/post")
 	public Greeting greeting(@RequestBody User user) {
 		return greetingService.addGreeting(user);
 	}
+	
 	/**
 	 * API for returning message by Id
 	 * @param id
-	 * @return
+	 * @return : Greeting message of particular id
 	 */
 	@GetMapping("/get/Id/{id}")
 	public Greeting gretting2(@PathVariable Long id){
 		return greetingService.getGreetingById(id);
 	}
+	
 	/**
 	 * API for getting all values in Repository
-	 * @return
+	 * @return : returns all Greeting messages
 	 */
 	@GetMapping("/get/all")
 	public List<Greeting> grettingAll(){
@@ -70,19 +67,19 @@ public class GreetController {
 	/**
 	 * API for delete value by Id
 	 * @param id
-	 * @return
+	 * @return : list of updated Greetings after deleting.
 	 */
-	@GetMapping("/get/delete/id/{id}")
+	@DeleteMapping("/delete/id/{id}")
 	public List<Greeting> deleteGreeting(@PathVariable Long id){
 		return greetingService.deletebyID(id);
 	}
 	/**
 	 * API for updating value by Id
 	 * @param greetingObj
-	 * @return
+	 * @return : update Greeting message.
 	 */
 	@PutMapping("/update/")
 	public Greeting update(@RequestBody Greeting greetingObj){
-		return greetingRepository.save(greetingObj);
+		return greetingService.saveUpdate(greetingObj);
 	}
 }
