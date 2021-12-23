dbb289c893823c0419cfdba95bc0d8de1aed87c7
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-12-23 23:05:25 +0530

    Added : Creating API to show Greeting message using Service Layer.

--- src/main/java/com/example/demo/controller/GreetController.java
+++ src/main/java/com/example/demo/controller/GreetController.java
@@ -31,11 +31,29 @@ public class GreetController {
 	@Autowired
 	private IGreetingService greetingService;
 	
-	@GetMapping("/greeting")
+	/**
+	 * API to Show Greeting Message by sending name as a parameter
+	 * @param name
+	 * @return : Json object with message id and message.
+	 */
+	@GetMapping("/getmessage/")
 	public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
 		return new Greeting(counter.incrementAndGet() , String.format(template, name));
 	}
 	
+	/**
+	 * API to show Greeting message by sending name as pathvariable
+	 * @param name
+	 * @return : Greeting message in Json with id and message.
+	 */
+	
+	@GetMapping ("/param/{name}")
+	public Greeting greeting2(@PathVariable String name) {
+		return new Greeting(counter.incrementAndGet() , String.format(template, name));	
+	}
+	/**
+	*API to show Greeting message using Service Layer
+	*/
 	@GetMapping("/hello")
 	public String hellomessage() {
 		GreetingService greeting = new GreetingService();
