335217119ce05899865d64d78412155e4ae193dd
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-12-23 22:27:35 +0530

    Added : Ability to save greeting messages in Repository.

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -1,10 +1,9 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
 import com.example.demo.Repository.GreatingRepository;
@@ -22,11 +21,11 @@ public class GreetingService implements IGreetingService {
 	
 	@Autowired
 	private GreatingRepository greetingRepository;
-	
+	/**
+	 * method to create Greeting message and save in Local repository
+	 */
 	@Override
 	public Greeting addGreeting(User user) {
 		String message = String.format(template, (user.toString().isEmpty()) ? " World " : user.toString());
 		return greetingRepository.save(new Greeting(counter.incrementAndGet(),message));
 	}
-		
-}
